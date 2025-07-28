99fa32e89a411207505042b8079c72f7d35cd92c
Juan Duque <144862881+JuanFDJ73@users.noreply.github.com>
2025-07-27 21:32:05 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home.jsx';
 import NotFound from './pages/not-found/NotFound.jsx';
 import Profile from './pages/profile/Profile.jsx';
-import Settings from './pages/settings/settings.jsx';
+import Settings from './pages/settings/Settings.jsx';
 import ProtectedRoute from './utils/ProtectedRoute.jsx';
 import Chat from './components/chat/chat.jsx';
 
