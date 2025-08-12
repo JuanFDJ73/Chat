f291e297169f65cca9326eabb3aafe1f15dd1245
JuanFDJ73 <vavodri@gmail.com>
2025-08-12 00:06:02 -0500

    fix: ChatMessages.jsx

--- frontend/src/components/chat/chatMessages/ChatMessages.jsx
+++ frontend/src/components/chat/chatMessages/ChatMessages.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MessageSent from '../messages/MessageSent.jsx';
-import MessageReceived from '../messages/MessageReceived.JSX';
+import MessageReceived from '../messages/MessageReceived.jsx';
 import './ChatMessages.css';
 
 const ChatMessages = () => {
